2b25d22f9813c1480eb6fc45a2521dfcb68d44b5
NoobYoup <134846824+NoobYoup@users.noreply.github.com>
2025-03-18 02:19:56 +0700

    soft delete

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+
+const route = require('./routes');
+const db = require('./config/db');
+
+//Connect to DB
+db.connect();
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+// HTTP logger
+app.use(morgan('combined'));
+
+app.use(methodOverride('_method'));
+
+// Template engine
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+// Route init
+route(app);
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
